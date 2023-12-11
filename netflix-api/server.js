1ab2415c421931ceee173b36eb27c097265ae4d3
Prince22 <112156035+Princebhan@users.noreply.github.com>
2023-12-11 19:01:29 +0530

    Update server.js

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -11,10 +11,10 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-// app.use(express.static(path.join(__dirname, "../netflix-ui/build")));
-// app.get("*", function (req, res) {
-//   res.sendFile(path.join(__dirname, "../netflix-ui/build/index.html"));
-// });
+ app.use(express.static(path.join(__dirname, "../netflix-ui/build")));
+ app.get("*", function (req, res) {
+   res.sendFile(path.join(__dirname, "../netflix-ui/build/index.html"));
+ });
 
 //mongoose.connect("mongodb://127.0.0.1:27017/netflix",{
 mongoose.connect("mongodb+srv://princebhandari:princebhandari@cluster0.agdo9ep.mongodb.net/netflix?retryWrites=true&w=majority",{
