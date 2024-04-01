5c1fe665666a0483d7028234067f7917a62905ab
Prince22 <112156035+princebhandarii@users.noreply.github.com>
2024-04-01 13:52:39 +0530

    Update server.js

--- netflix-api/server.js
+++ netflix-api/server.js
@@ -10,6 +10,7 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use("/api/user", userRoutes);
 
  app.use(express.static(path.join(__dirname, "../netflix-ui/build")));
  app.get("*", function (req, res) {
@@ -30,5 +31,4 @@ mongoose.connect("mongodb+srv://princebhandari:princebhandari@cluster0.agdo9ep.m
 //});
 
 
-app.use("/api/user", userRoutes);
 app.listen(8000, console.log("server started "));
